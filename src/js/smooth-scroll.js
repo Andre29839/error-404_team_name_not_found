b81a4dd7f26df2cb353b8f6945876fa444ac6693
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-08-07 13:46:02 +0300

    add first correction

--- src/js/smooth-scroll.js
+++
@@ -1,1 +0,0 @@
-new SmoothScroll('a[href*="#"]');
--- src/main.js
+++ src/main.js
@@ -1,4 +1,2 @@
-import './js/smooth-scroll.js';
-import './js/animation-header.js';
 import './js/animation-scroll.js';
 import './js/mobile-menu.js';
