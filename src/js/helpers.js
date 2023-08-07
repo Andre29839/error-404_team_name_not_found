7f0a2797d3a25e34a44d36d2c232bc585a56a47f
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-08-07 22:14:03 +0300

    add additional options

---
+++ src/js/helpers.js
@@ -0,0 +1,10 @@
+const refs = {
+    API_KEY: "c3755e1c88b3f430b9a9356edda9caa4",
+    BASIC_URL: "https://developers.themoviedb.org/3",
+    trending: "/trending/get-trending",
+    new_films: "/movies/get-upcoming",
+    search_films: "/search/search-movies",
+    movie_detailes: "/movies/get-movie-details",
+    trailer: "/movies/get-movie-videos",
+    genger: "/genres/get-movie-list",
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,5 @@
-import './animation-scroll.js';
-import './mobile-menu.js';
+import "./animation-scroll.js";
+import "./mobile-menu.js";
+import "./helpers.js";
+import "./library.js";
+import "./catalog.js";
