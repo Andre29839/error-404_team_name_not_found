9507a994f5aa49aa5f13624d9f27c4eccdb48cea
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-08-09 21:56:54 +0300

    add additional options

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import "./animation-scroll.js";
 import "./mobile-menu.js";
 import "./helpers.js";
 import "./library.js";
-import "./catalog.js";
 import "./modal-menu.js"
 import "./hero_markup.js";
 import "./hero_app.js";
