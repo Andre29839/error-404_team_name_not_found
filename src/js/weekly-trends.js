c75bbf0dcbfc0398b91ceeee3105357414816c85
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-08-10 22:04:11 +0300

    add-branch

--- src/js/weekly-trends.js
+++ src/js/weekly-trends.js
@@ -9,16 +9,18 @@ const refsMonth = {
   wrapper: document.querySelector('.month-wrapper'),
   addToLibraryBtn: document.querySelector('.add-to-library'),
   container: document.querySelector('.container-img-weekly'),
+  removeLibraryBtn:document.querySelector('.remove-from-library'),
 };
 
 refsMonth.addToLibraryBtn.addEventListener('click', onOpenLibraryBtn);
 window.addEventListener('resize', onResizeDisplay);
+refsMonth.removeLibraryBtn.addEventListener('click',onRemoveFromLibrary)
 
 let filmInStorage = JSON.parse(localStorage.getItem(LIBRARY_KEY)) || [];
 let toStorage;
 
 async function fetchTrendingMonthMovies() {
-  const url = `${BASIC_URL}${trending_week}?api_key=${API_KEY}`;
+  const url = `${BASIC_URL}${new_films}?api_key=${API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
   const randomIndex = Math.floor(Math.random() * data.results.length);
@@ -29,7 +31,16 @@ async function fetchTrendingMonthMovies() {
 function onOpenLibraryBtn() {
   filmInStorage.push(toStorage);
   localStorage.setItem(LIBRARY_KEY, JSON.stringify(filmInStorage));
+  refsMonth.addToLibraryBtn.classList.add('visually-hidden')
+  refsMonth.removeLibraryBtn.classList.remove('visually-hidden')
 }
+
+function onRemoveFromLibrary(){
+  refsMonth.addToLibraryBtn.classList.remove('visually-hidden')
+  refsMonth.removeLibraryBtn.classList.add('visually-hidden');
+  localStorage.removeItem(LIBRARY_KEY)
+}
+
 
 function onResizeDisplay() {
   if (window.innerWidth === 768) {
@@ -37,7 +48,18 @@ function onResizeDisplay() {
   }
 }
 
-let RESULTS_PER_PAGE = 1;
+const GENRES_URL = "/genre/movie/list"
+
+let res;
+
+async function fetchGenres(){
+  const urlGenre = `${BASIC_URL}${GENRES_URL}?api_key=${API_KEY}`
+  const response = await fetch(urlGenre);
+  const data = await response.json();
+  res = data.genres.map(({name}) => name);
+  console.log(res);
+}
+fetchGenres()
 
 async function fetchTrendingMovies() {
   const url = `${BASIC_URL}${trending_week}?api_key=${API_KEY}`;
@@ -70,14 +92,14 @@ renderAndAppendMarkup();
 function renderPageMarkup(array) {
   const result = array
     .map(
-      ({ title, id, backdrop_path, genre_ids, release_date }) =>
-        `<div data-id="${id}" class="container-img-weekly">
-        <img src="${IMG_URL}${backdrop_path}" alt="" class="img-weekly">
+      ({ title, id, poster_path, genre_ids, release_date,overview }) =>
+        `<li data-id="${id}" class="container-img-weekly">
+        <img src="${IMG_URL}${poster_path}" alt="${overview}" loading="lazy" class="img-weekly">
         <div class="img-wrapper">
             <h3 class="title-cinema">${title}</h3>
             <p class="genre-year-text">${genre_ids} | ${release_date}</p>
         </div>
-    </div>`
+    </li>`
     )
     .join('');
   return result;
@@ -107,7 +129,7 @@ function renderTrendingMonthMarkup(array) {
         popularity,
         genre_ids,
         overview,
-      }) => `<div data-id="${id}" class="js-card-month">
+      }) => `<li data-id="${id}" class="js-card-month">
 <img src="${IMG_URL}${backdrop_path}" alt="" class="month-img">
 <h4 class="month-title-movie">${title}</h4>
 <div class="wrapper-month-section">
@@ -126,7 +148,7 @@ function renderTrendingMonthMarkup(array) {
 </div>
 <p class="about-text">ABOUT</p>
 <p class="description-text">${overview}</p>
-</div>
+</li>
 `
     )
     .join('');
