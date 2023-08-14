47ff9080f38b43eaca295b588994fa7ec49a92d3
DianaShylova <torfinn.karlseffny@gmail.com>
2023-08-14 09:48:56 +0300

    Fixig close modal button

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -8,29 +8,37 @@
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", closeBtnFtrModal);
 
-  refs.backdrop.addEventListener("click", toggleModal);
+  refs.backdrop.addEventListener("click", closeBtnFtrModal);
 
   function closeFooterModal(evt) {
     
     if (evt.code === "Escape") {
-      toggleModal();
+      closeBtnFtrModal();
     }
   }
-    
-  function toggleModal(evt) {
+  
+  function closeBtnFtrModal() {
+    toggleModal(false, false);
+  }
+  function toggleModal(evt, show) {
     if (evt) { 
-      evt.preventDefault();
+     /*  evt.preventDefault(); */
     }
     
     let scrollTop = document.scrollingElement.scrollTop;
     
-    
+    /* refs.modal.style.top = "calc(" + scrollTop + "px + 20px)"; */
     refs.backdrop.style.top = "calc(" + scrollTop + "px)";
-
-    refs.modal.classList.toggle("visually-hidden");
-    refs.backdrop.classList.toggle("visually-hidden");
+    if (show === false) {
+      refs.modal.classList.add("visually-hidden");
+      refs.backdrop.classList.add("visually-hidden");
+    } else {
+      refs.modal.classList.toggle("visually-hidden");
+      refs.backdrop.classList.toggle("visually-hidden");
+    }
+    
     
         if (refs.backdrop.classList.contains("visually-hidden")) {
       document.removeEventListener("keydown", closeFooterModal)
