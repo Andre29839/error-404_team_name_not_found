6d41f25082d5e4d8a22aea462469e1b4cd9f29a3
Andrii Krytsia <124679055+AndriiKrytsia@users.noreply.github.com>
2023-08-20 21:05:39 +0200

    11111

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,5 +1,6 @@
 import Pagination from 'tui-pagination';
-import { refs, getGenres } from "./helpers";
+import { refs, getGenres } from './helpers';
+import { defaultImage } from '../images/defaultImage.jpg';
 
 const { API_KEY, BASIC_URL, search_films, trending_week, new_films } = refs;
 
@@ -21,17 +22,16 @@ let userParams = {
   year: '',
   query: '',
   page: 1,
-}
-let resultsArr; 
-  
+};
+let resultsArr;
+
 searchForm.addEventListener('submit', onSubmitForm);
 svgReset.addEventListener('click', onClickSvgReset);
 
 function onClickSvgReset() {
   searchForm.reset();
- svgReset.classList.add("visually-hidden");
+  svgReset.classList.add('visually-hidden');
 }
-
 
 const options = {
   totalItems: totalResults,
@@ -40,7 +40,8 @@ const options = {
   centerAlign: true,
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
@@ -52,8 +53,8 @@ const options = {
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
+      '</a>',
+  },
 };
 
 async function fetchFilms() {
@@ -65,23 +66,20 @@ async function fetchFilms() {
   return response;
 }
 
+// const defaultUrl =
+//   'https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg';
 async function createDefaultMarkup(pictures) {
-  let markupLibrary ='';
+  let markupLibrary = '';
   for (const elem of pictures) {
-    const {
-      title,
-      id,
-      poster_path,
-      release_date,
-      overview,
-      vote_average,
-    } = elem;
-const movieWeekGenre = await getGenres(id);
-const WEEK_IMG_URL = 'https://image.tmdb.org/t/p/original/';
-markupLibrary += `<li class="movie-card open-modal" data-movie-id="${id}">
+    const { title, id, poster_path, release_date, overview, vote_average } =
+      elem;
+    console.log(poster_path);
+    const movieWeekGenre = await getGenres(id);
+    const WEEK_IMG_URL = 'https://image.tmdb.org/t/p/original/';
+
+    markupLibrary += `<li class="movie-card open-modal" data-movie-id="${id}">
          <div class="gradient"></div>
-         <img class="movie-img" src="${WEEK_IMG_URL}${poster_path
-         }" alt="${overview}" loading="lazy" />
+         <img class="movie-img" src="${WEEK_IMG_URL}${poster_path}" alt="${overview}" loading="lazy" />
          <div class="info">
         <div class="name-and-discr">
          <p class="movie-title">
@@ -94,58 +92,54 @@ markupLibrary += `<li class="movie-card open-modal" data-movie-id="${id}">
       <div class="rating-active" style="width:${vote_average * 10}%"></div>
     </div>
   </div>
-         </li>`}
+         </li>`;
+  }
   gallery.innerHTML = markupLibrary;
-  
- const first = document.querySelector(".tui-first");
- const last = document.querySelector(".tui-last");
- const first_first = document.querySelector(".tui-next")
- const last_last = document.querySelector(".tui-prev")
- const elipse = document.querySelector(".tui-next-is-ellip")
 
-  first.classList.remove("tui-page-btn")
-  last.classList.remove("tui-page-btn")
-  first_first.classList.remove("tui-page-btn")
-  last_last.classList.remove("tui-page-btn")
-  elipse.classList.remove("tui-page-btn")
-};
+  const first = document.querySelector('.tui-first');
+  const last = document.querySelector('.tui-last');
+  const first_first = document.querySelector('.tui-next');
+  const last_last = document.querySelector('.tui-prev');
+  const elipse = document.querySelector('.tui-next-is-ellip');
 
+  first.classList.remove('tui-page-btn');
+  last.classList.remove('tui-page-btn');
+  first_first.classList.remove('tui-page-btn');
+  last_last.classList.remove('tui-page-btn');
+  elipse.classList.remove('tui-page-btn');
+}
 
-   searchForm.elements.searchQuery.addEventListener("focus", function() {
-  svgReset.classList.remove("visually-hidden");
-  
+searchForm.elements.searchQuery.addEventListener('focus', function () {
+  svgReset.classList.remove('visually-hidden');
 });
 
-
- async function onSubmitForm(e) {
+async function onSubmitForm(e) {
   e.preventDefault();
-   isActiveFetch = true;
-   typeFetch = 'searchmovies';
+  isActiveFetch = true;
+  typeFetch = 'searchmovies';
 
   const dataText = searchForm.elements.searchQuery.value;
   input = dataText;
-   
-   e.target.reset();
-   svgReset.classList.add("visually-hidden");
-   gallery.innerHTML = '';
-   
-   
+
+  e.target.reset();
+  svgReset.classList.add('visually-hidden');
+  gallery.innerHTML = '';
+
   if (dataText.trim() === '') {
     const oopsMarkup = `<p class="oops-text">OOPS...<br>
      We are very sorry!<br>
-     We don’t have any results matching your search.</p>`
+     We don’t have any results matching your search.</p>`;
     gallery.innerHTML = oopsMarkup;
-    paginationDiv.classList.add("visually-hidden");
+    paginationDiv.classList.add('visually-hidden');
     return;
   }
-  paginationDiv.classList.remove("visually-hidden");
-  
+  paginationDiv.classList.remove('visually-hidden');
 
   userParams.query = input;
   userParams.page = 1;
-   const total = await appendMarkup();
-   paginationInstance.reset(total);
-   isActiveFetch = false;
+  const total = await appendMarkup();
+  paginationInstance.reset(total);
+  isActiveFetch = false;
 }
 
 function updatePaginationMarkup(currentPage) {
@@ -162,91 +156,90 @@ function updatePaginationMarkup(currentPage) {
 let currentPage;
 let movies = [];
 let isActiveFetch = false;
-let typeFetch = 'trending'; 
-
+let typeFetch = 'trending';
 
 async function loadMoviesForPage(page) {
   try {
     userParams.page = page;
-    const response = await fetch(`${BASIC_URL}${search_films}?api_key=${API_KEY}&query=${userParams.query}&page=${page}`);
+    const response = await fetch(
+      `${BASIC_URL}${search_films}?api_key=${API_KEY}&query=${userParams.query}&page=${page}`
+    );
     const moviesData = await response.json();
     movies = moviesData.results;
-    
+
     if (moviesData.totalPages == userParams.page) {
-  paginationDiv.classList.add("visually-hidden");
-}
-   createDefaultMarkup(movies);
+      paginationDiv.classList.add('visually-hidden');
+    }
+    createDefaultMarkup(movies);
 
     totalPages = moviesData.total_pages;
-
   } catch (error) {
     console.log(error);
   }
 }
 
-
 let paginationInstance = new Pagination(pagContainer, options);
 paginationInstance.on('afterMove', e => {
   if (typeFetch === 'trending') {
     loadTrendingMovies(e.page);
-      sectionContainer.scrollIntoView({ behavior: "smooth" });
+    sectionContainer.scrollIntoView({ behavior: 'smooth' });
     return;
-  };
+  }
 
-      const currentPage = e.page;
-      userParams.page = currentPage;
+  const currentPage = e.page;
+  userParams.page = currentPage;
 
-      loadMoviesForPage(currentPage);
-      updatePaginationMarkup(currentPage);
+  loadMoviesForPage(currentPage);
+  updatePaginationMarkup(currentPage);
 });
-    
+
 async function appendMarkup() {
   try {
     const movies = await fetchFilms();
     resultsArr = movies.total_results || 0;
- if (resultsArr === 0) {
+    if (resultsArr === 0) {
       const oopsMarkup = `<p class="oops-text">OOPS...<br>
         We are very sorry!<br>
         We don’t have any results matching your search.</p>`;
       gallery.innerHTML = oopsMarkup;
-      paginationDiv.classList.add("visually-hidden");
+      paginationDiv.classList.add('visually-hidden');
       return;
     }
-   createDefaultMarkup(movies.results);
+    createDefaultMarkup(movies.results);
 
     if (!userParams.query) {
       loadMoviesForPage(currentPage);
     }
-    
   } catch (error) {
     console.log(error);
   }
   return resultsArr;
 }
 
-
 async function onLoad() {
- const total = await loadTrendingMovies();
+  const total = await loadTrendingMovies();
   paginationInstance.reset(total);
 }
 onLoad();
 
-async function loadTrendingMovies(page=1) {
+async function loadTrendingMovies(page = 1) {
   try {
-    const response = await fetch(`${BASIC_URL}${trending_week}?api_key=${API_KEY}&page=${page}`);
+    const response = await fetch(
+      `${BASIC_URL}${trending_week}?api_key=${API_KEY}&page=${page}`
+    );
     const moviesData = await response.json();
     movies = moviesData.results;
 
-   createDefaultMarkup(movies);
+    createDefaultMarkup(movies);
     options.totalItems = moviesData.total_results;
     totalResults = moviesData.total_results;
-return totalResults
+    return totalResults;
   } catch (error) {
-   const oopsMarkup = `<p class="oops-text">OOPS...<br>
+    const oopsMarkup = `<p class="oops-text">OOPS...<br>
      We are very sorry!<br>
-     Something went wrong!</p>`
-         gallery.innerHTML = oopsMarkup;
-    paginationDiv.classList.add("visually-hidden");
-    return; 
+     Something went wrong!</p>`;
+    gallery.innerHTML = oopsMarkup;
+    paginationDiv.classList.add('visually-hidden');
+    return;
   }
 }
